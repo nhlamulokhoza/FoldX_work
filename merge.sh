bb8d21e692a6e3ed0115962fc5701f7101bf8c25
nhlamulokhoza <79716218+nhlamulokhoza@users.noreply.github.com>
2021-12-05 09:25:18 +0200

    Update merge.sh

--- merge.sh
+++ merge.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-ls CYP*vcf | sed 's/.vcf//' > haplotypes.txt
+ls CYP*vcf | sed 's/.vcf//' > haplotypes.txt               # List all vcfs and redirect into haplotype.txt -- Each vcf corresponds to a haplotype
 
-for i in $(cat haplotypes.txt); do
+for i in $(cat haplotypes.txt); do                         # Compress and index vcf files listed in haplotype.txt
     bgzip ${i}.vcf
     tabix ${i}.vcf.gz
 done
 
-bcftools merge -m none *.vcf.gz > haplotypes.vcf 
+bcftools merge -m none *.vcf.gz > haplotypes.vcf           # Merge all compressed and indexed vcfs into one -- preparation for VeP
